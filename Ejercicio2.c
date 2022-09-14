f905c793e6aed0e021069b31ab1a423c44ca7fab
Ignacio Granados <ignaciogramar@gmail.com>
2022-09-14 13:46:39 -0600

    Updating exercise 2

--- Ejercicio2.c
+++ Ejercicio2.c
@@ -1,45 +1,42 @@
-// Ejercicio 2
-
-
+// libraries
 #include <stdio.h>
-//#include <stdlib.h>
 #include <pthread.h>
 
-void *functionC();
-pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
-int  counter = 5;
+// global variables
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+int sharedCounter = 0;
 
-main()
-{
-   int rc1, rc2;
-   pthread_t thread1, thread2;
-
-   /* Create independent threads each of which will execute functionC */
-
-   if( (rc1=pthread_create( &thread1, NULL, &functionC, NULL)) )
-   {
-      printf("Thread creation failed: %d\n", rc1);
-   }
-
-   if( (rc2=pthread_create( &thread2, NULL, &functionC, NULL)) )
-   {
-      printf("Thread creation failed: %d\n", rc2);
-   }
-
-   /* Wait till threads are complete before main continues. Unless we  */
-   /* wait we run the risk of executing an exit which will terminate   */
-   /* the process and all threads before the threads have completed.   */
-
-   pthread_join( thread1, NULL);
-   pthread_join( thread2, NULL); 
-
-   exit(0);
+// this functions belogns to both threads
+void *threadsFunction() {
+	
+	printf("Inicio del hilo\n");
+		
+	// lock resource
+	pthread_mutex_lock(&mutex);	
+	
+	sharedCounter ++;
+   	
+   	printf("Contador compartido = %d\n", sharedCounter);
+   	
+   	// unlock resource
+   	pthread_mutex_unlock(&mutex);
+   	
+   	printf("Fin del hilo\n\n");
+	    
 }
 
-void *functionC()
-{
-   pthread_mutex_lock( &mutex1 );
-   counter++;
-   printf("Counter value: %d\n",counter);
-   pthread_mutex_unlock( &mutex1 );
+int main() {
+	
+	pthread_t thread1;
+	pthread_t thread2;
+	
+	int write1 = pthread_create(&thread1, NULL, threadsFunction, NULL);
+	int write2 = pthread_create(&thread2, NULL, threadsFunction, NULL);
+
+	pthread_join(thread1, NULL);
+	pthread_join(thread2, NULL); 
+
+	return 0;
+     
 }
+
